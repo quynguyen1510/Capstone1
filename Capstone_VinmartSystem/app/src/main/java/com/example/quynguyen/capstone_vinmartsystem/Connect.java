03923df1d8ece7074fedc3a4fd1d5b53efc0af20
Quy Nguyen <quycao1510@gmail.com>
2018-11-14 15:06:29 +0700

    Update Maps for delivery

--- Capstone_VinmartSystem/app/src/main/java/com/example/quynguyen/capstone_vinmartsystem/Connect.java
+++ Capstone_VinmartSystem/app/src/main/java/com/example/quynguyen/capstone_vinmartsystem/Connect.java
@@ -1,5 +1,6 @@
 package com.example.quynguyen.capstone_vinmartsystem;
 
 public class Connect {
+  //  String urlData = "http://10.80.124.213:8080/androidwebservice";
     String urlData = "https://vinmart-systemvn.000webhostapp.com";
 }
