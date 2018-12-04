72de2a75ee06f747c69c62012f2bc9ea39d577cf
Quy Nguyen <quycao1510@gmail.com>
2018-12-04 14:52:15 +0700

    Update combobox and validate update password

--- Capstone_VinmartSystem/app/src/main/java/com/example/quynguyen/capstone_vinmartsystem/MapActivity.java
+++ Capstone_VinmartSystem/app/src/main/java/com/example/quynguyen/capstone_vinmartsystem/MapActivity.java
@@ -94,13 +94,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             maps.addMarker(new MarkerOptions().title("Vinmart Hải Châu").snippet("Hệ thống cửa hàng Vinmart").position(cusAddress));
         }
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         maps.setMyLocationEnabled(true);
